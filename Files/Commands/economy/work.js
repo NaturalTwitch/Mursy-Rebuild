18e5d550befc5762a7796a854fe6e92ebff92186
NaturalTwitch <awesomejunior2@gmail.com>
2025-09-22 11:50:50 -0400

    Creation of Economy Command File

---
+++ Files/Commands/economy/work.js
@@ -0,0 +1,11 @@
+const { EmbedBuilder } = require('discord.js');
+const userCooldowns = new Map();
+
+module.exports = {
+    name: 'work',
+    description: 'Work to earn some coins!',
+    utilisation: '{prefix}work',
+    async execute(client, message, cmd, args, Discord) {
+        
+    }
+}
